cad5f4737acba9696326361273419b920d8c742b
x <exit>
2022-10-11 18:11:23 +0000

    Uodate index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+/* eslint-disable linebreak-style */
 import './styles.css';
+
+const score =[
+  { user: 'John', score: 70 },
+  { user: 'Frank', score: 80 },
+  { user: 'Mike', score: 50 },
+  { user: 'James', score: 46 },
+  { user: 'Ama', score: 77 },
+  { user: 'Cecilia', score: 67 },
+];
+
+const listTag = document.querySelector('ul');
+console.log(listTag)
